76064b2942b847f28ce6edf88611a0a286b1a267
Mupparaju19 <143019131+Mupparaju19@users.noreply.github.com>
2023-11-26 20:37:23 -0600

    latest commit

--- models/wolf.js
+++ models/wolf.js
@@ -1,7 +1,18 @@
 const mongoose = require("mongoose")
 const wolfSchema = mongoose.Schema({
     wolf_name: String,
-    wolf_age: Number,
-    wolf_price: Number
+    wolf_age: {
+        type: String,
+        required: true,
+        minlength: 0, 
+        maxlength: 50,
+       
+      },
+    wolf_price: {
+        type: Number,
+        required: true,
+        min: 5,
+        max: 1000,
+      },
 })
 module.exports = mongoose.model("wolf",wolfSchema)
--- routes/wolf.js
+++ routes/wolf.js
@@ -1,18 +1,28 @@
 var express = require('express');
 const wolf_controlers= require('../controllers/wolf');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ res.redirect("/login");
+ }
 /* GET costumes */
 router.get('/', wolf_controlers.wolf_view_all_Page );
 
 /* GET detail wolf page */
-router.get('/detail', wolf_controlers.wolf_view_one_Page);
+router.get('/detail',secured, wolf_controlers.wolf_view_one_Page);
 
 /* GET create costume page */
-router.get('/create', wolf_controlers.wolf_create_Page);
+router.get('/create',secured, wolf_controlers.wolf_create_Page);
 
 /* GET create update page */
-router.get('/update', wolf_controlers.wolf_update_Page);
+router.get('/update',secured, wolf_controlers.wolf_update_Page);
 
 /* GET delete costume page */
-router.get('/delete', wolf_controlers.wolf_delete_Page);
+router.get('/delete',secured, wolf_controlers.wolf_delete_Page);
 module.exports = router;
